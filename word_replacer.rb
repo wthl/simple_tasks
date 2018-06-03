277a0372aefd01998c564f63fd998629fb1f278f
wthl <wthl3000@gmail.com>
2018-06-03 19:37:12 +0300

    add word_replacer

---
+++ word_replacer.rb
@@ -0,0 +1,7 @@
+s = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+
+puts s
+
+puts "        "
+
+puts s.split(/ /).reverse.join(' ')
